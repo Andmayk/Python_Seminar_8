b671efb454006e08518a414f49c0bd9214f95076
AndMay <am@42.ru>
2023-09-16 12:24:11 +0700

    modified search

--- phone_book.py
+++ phone_book.py
@@ -50,7 +50,7 @@ def interface_contact():
     while interfeis_act != '0':
         match interfeis_act:
             case '1':
-                find_contact()
+                find_in_phone_book()
             case '2':
                 add_contact()
             case '3':
@@ -74,11 +74,11 @@ def interface_contact():
         save_to_file(phone_list_file)
 
 def sort_list():
+    phone_book_list.sort()
     global flag_of_change
     flag_of_change = True
-    phone_book_list.sort()
 
-def yes_no(message_y_n = "Да/Нет (д/н | y/n): "):
+def yes_no(message_y_n = "Да/Нет (д/н | y/n): ") -> bool:
     ansver = input(message_y_n)
     if ansver.upper() in ('Y','YES','Д','ДА','1'):
         return True
@@ -98,39 +98,53 @@ def add_contact():
     last_name, first_name, patronim, telefon = input_contact()
     print(f"Добавить контакт {contact_for_print(last_name.title() + ', ' +  first_name.title() + ', ' +patronim.title()+ ', '+  telefon)}? ")
     if yes_no():
+        phone_book_list.append(last_name.title() + ', ' +  first_name.title() + ', ' +patronim.title()+ ', '+  telefon)
         global flag_of_change
         flag_of_change = True
-        phone_book_list.append(last_name.title() + ', ' +  first_name.title() + ', ' +patronim.title()+ ', '+  telefon)
 
-def find_contact():
-    find_name = input('Поиск по всем полям: ').upper()
+def find_contact(str_find, position = 0):
+    str_find = str_find.upper()
+    if position < len(phone_book_list):
+        for i in range(position, len(phone_book_list)):
+            if str_find in phone_book_list[i].upper():
+                # print('проверка поиска : ', i, str_find, phone_book_list[i].upper())
+                return i
+    return None
+
+def find_in_phone_book():
+    find_str = input('Введите строку для поиск по всем полям без учета регистра: ')
     none_contact = True
-    j = 0
-    for i in phone_book_list:
-        if find_name in i.upper():
-            print('Контакт найден:', j, contact_for_print(i))
-            none_contact = False
-        j += 1
+    p = find_contact(find_str, 0)
+    while p != None:
+        print('Контакт найден:', p, contact_for_print(phone_book_list[p]))
+        none_contact = False
+        p = find_contact(find_str, p+1)
     if none_contact:
         print('Контакт не найден')
-
 
 def del_find_contact():
-    find_name = input('Поиск контакта для удаления: ').upper()
+    print('ПОИСК И УДАЛЕНИЕ КОНТАКТА')
+    find_str = input('Введите строку для поиск по всем полям без учета регистра: ')
     none_contact = True
-    j = 0
-    for i in phone_book_list:
-        if find_name in i.upper():
-            print('Контакт найден')
-            none_contact = False
-            print('Удалить контакт?', j, contact_for_print(i))
-            if yes_no():
-                phone_book_list.remove(i)
-                global flag_of_change
-                flag_of_change = True
-        j += 1
+    p = find_contact(find_str, 0)
+    while p != None:
+        none_contact = False
+        found_contact = contact_for_print(phone_book_list[p])
+        print('Контакт найден:', p, found_contact)
+        print('Удалить контакт?')
+        if yes_no():
+            phone_book_list.remove(phone_book_list[p])
+            print(f"Контакт {found_contact} удален.")
+            global flag_of_change
+            flag_of_change = True
+        print(f"Продолжить поиск строки {find_str} ?")
+        if yes_no():
+            none_contact = True
+            p = find_contact(find_str, p+1)
+        else:
+            p = None
     if none_contact:
-        print('Контакт не найден')
+        print('Контакты не найдены')
 
 def del_contact_by_index():
     find_index = int(input('Введите индекс строки контакта для удаления: '))
@@ -144,25 +158,28 @@ def del_contact_by_index():
         print('Контакт не найден')
 
 def edit_find_contact():
-    find_name = input('Поиск контакта для измененияния: ').upper()
+    print('ПОИСК И ИЗМЕНЕНИЕ КОНТАКТА')
+    find_str = input('Введите строку для поиск по всем полям без учета регистра: ')
     none_contact = True
-    j = 0
-    for i in phone_book_list:
-        if find_name in i.upper():
-            print('Найден контакт:', j, contact_for_print(i))
-            none_contact = False
-            old_contact = contact_for_print(i)
-            print('Изменить?', j, old_contact)
-            if yes_no():
-                last_name, first_name, patronim, telefon = input_contact()
-                print("контакт", old_contact)
-                # i = last_name.upper() + ', ' +  first_name.upper() + ', ' +patronim.upper()+ ', '+  telefon
-                phone_book_list[j] = last_name.title() + ', ' +  first_name.title() + ', ' +patronim.title()+ ', '+  telefon
-                print(f"заменен на: {contact_for_print(last_name.title() + ', ' +  first_name.title() + ', ' +patronim.title()+ ', '+  telefon)}")
-           
-                global flag_of_change
-                flag_of_change = True
-        j += 1
+    p = find_contact(find_str, 0)
+    while p != None:
+        none_contact = False
+        found_contact = contact_for_print(phone_book_list[p])
+        print('Контакт найден:', p, found_contact)
+        print('Изменить?')
+        if yes_no():
+            last_name, first_name, patronim, telefon = input_contact()
+            print("контакт", found_contact)
+            phone_book_list[p] = last_name.title() + ', ' +  first_name.title() + ', ' +patronim.title()+ ', '+  telefon
+            print(f"заменен на: {contact_for_print(last_name.title() + ', ' +  first_name.title() + ', ' +patronim.title()+ ', '+  telefon)}")
+            global flag_of_change
+            flag_of_change = True
+        print(f"Продолжить поиск строки {find_str} ?")
+        if yes_no():
+            none_contact = True
+            p = find_contact(find_str, p+1)
+        else:
+            p = None
     if none_contact:
         print('Контакт не найден')    
 
